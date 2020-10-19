1fc78015d134f657698eda27ff4bc7abd3b05b50
Morten Jørgensen <mr.mjoergensen@gmail.com>
2020-10-19 12:14:11 +0200

    Added a new print statement

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("First GIT");
         System.out.println("This is a new branch");
+
+        //morten test
+        System.out.println("This is a branch from morten");
     }
 }
