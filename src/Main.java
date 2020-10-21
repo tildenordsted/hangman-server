14d594807e4781f8ea8678712f0bc235a10d8611
Mikkel Siercke <mikkelsiercke@outlook.com>
2020-10-21 11:30:20 +0200

    Tilføjet en simpel server der kan oprette forbindelse til klienten

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,31 @@
+//Impoter biblioteker
+import java.io.*; //Den sørger for at systemet kan lave input og output gennem "data streams"
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.ServerSocket;
+
 public class Main {
-
     public static void main(String[] args) {
-        System.out.println("First GIT");
-        System.out.println("This is a new branch");
+        try {
+            //Opretter et server socket objekt med vores port/"dørnøgle"
+            ServerSocket serverSocket = new ServerSocket(6666);
 
-        //morten test
-        System.out.println("This is a branch from morten");
+            //Etablerer forbindelsen til klienten
+            Socket connectToClient = serverSocket.accept();
+
+            DataInputStream inputFromClient = new DataInputStream(connectToClient.getInputStream());
+            DataOutputStream outFromServer = new DataOutputStream(connectToClient.getOutputStream());
+
+            String stringInput = (String)inputFromClient.readUTF();
+            System.out.println("Brugernavn: " + stringInput + " er forbundet til server");
+
+            outFromServer.writeUTF("Du har oprettet forbindelse til server");
+
+            serverSocket.close();
+
+
+        } catch (IOException e) {
+            System.out.println(e);
+        }
     }
 }
