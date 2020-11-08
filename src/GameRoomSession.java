bf98baa0c177ecc4dd537201c13dfe7d5173351b
HalfdanIsaksen <56478241+HalfdanIsaksen@users.noreply.github.com>
2020-11-08 21:23:50 +0100

    Update GameRoomSession.java

--- src/GameRoomSession.java
+++ src/GameRoomSession.java
@@ -10,13 +10,18 @@ public class GameRoomSession implements Runnable{
     Boolean isRunning = true;
     Boolean showScore = true;
 
+    private WordDatabase wordDatabase;
+
     //private ArrayList<ClientHandler> clients = new ArrayList<>();
 
     private List<ClientHandler> clients = Collections.synchronizedList(new ArrayList<ClientHandler>());
 
 
     //constructor
-    public GameRoomSession(){}
+    public GameRoomSession(){
+        wordDatabase = new WordDatabase();
+        wordDatabase.setString(wordDatabase.randomWord());
+    }
 
     //Getter and setters
 
@@ -55,7 +60,7 @@ public class GameRoomSession implements Runnable{
     public void run() {
 
         while(isRunning){
-            if(clients.size() == 2){
+            if(clients.size() == 3){
                 try{
                     if(showScore){
                         writeToAllInRoom(new Message(getClientListNamesAndLives(), "updateusers"));
@@ -64,19 +69,29 @@ public class GameRoomSession implements Runnable{
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-            }
-
-            if(clients.get(0).getGuess() != null) {
-                try {
-                    writeToAllInRoom(new Message(clients.get(0).getGuess(), "newguess"));
-                    clients.get(0).setGuess(null);
-                } catch (IOException e) {
-                    e.printStackTrace();
+                for(int i = 0; i < clients.size(); i++) {
+                    //if(clients.get(i) != clients.get(2)) {
+                        if (clients.get(i).getLives() == clients.get(2).getLives()) {
+                            if (clients.get(i).getGuess() != null) {
+                                try {
+                                    wordDatabase.checkString(clients.get(i).getGuess(), wordDatabase);
+                                    clients.get(i).setLives(clients.get(i).getLives()-wordDatabase.getMinusLives());
+                                    wordDatabase.setMinusLives(0);
+                                    System.out.println(clients.get(0).getLives());
+                                    System.out.println(clients.get(1).getLives());
+                                    System.out.println(clients.get(2).getLives());
+                                    System.out.println(wordDatabase.getMinusLives());
+                                    writeToAllInRoom(new Message(clients.get(i).getGuess(), "newguess"));
+                                    clients.get(i).setGuess(null);
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }
                 }
             }
         }//end while isRunning
     } //end run
 }
-
 
 
